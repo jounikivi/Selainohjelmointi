5163c5e4c5f29333ff06d2d97a48a23f01230e1e
Jouni Kiviperä <jounikivipera@gmail.com>
2023-09-16 11:21:45 +0300

    mu

--- JavaScript/Koodit/esimerkejä.js
+++ JavaScript/Koodit/esimerkejä.js
@@ -1,6 +1,7 @@
-// Esimerkki1 tehtävään 3 b
+// Esimerkki tehtävään 3 b
 var määrä = 36.5;
 var text1 = "Minulla on";
-var text2 = "eruoa";
-var tulos = text1 + määrä + text2;
+var text2 = " eruoa";
+var tulos = `${text1} ${määrä}${text2}`;
+//var tulos = text1 + määrä + text2;
 console.log(tulos);
