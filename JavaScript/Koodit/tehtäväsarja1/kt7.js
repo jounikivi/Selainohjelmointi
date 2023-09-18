1468e0ccd087a0df9058ea878d63185db9f89a80
Jouni Kiviperä <jounikivipera@gmail.com>
2023-09-18 09:27:29 +0300

    Update kt7.js

--- JavaScript/Koodit/tehtäväsarja1/kt7.js
+++ JavaScript/Koodit/tehtäväsarja1/kt7.js
@@ -3,10 +3,10 @@ const points = [64, 56, 48, 12, 81, 91, 34, 19, 95, 55];
 const grades = points.map((ptn) => {
 
   if (ptn >= 85) {
-    return 5;
+    return 4;
   }
   else if (ptn >= 70) {
-    return 4
+    return 3
   }
   else if (ptn >= 60) {
     return 2
@@ -14,5 +14,5 @@ const grades = points.map((ptn) => {
     return 1
   }
 });
-console.log(points)
+console.log(points);
 console.log(grades);
