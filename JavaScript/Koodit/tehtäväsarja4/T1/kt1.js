b57229bd704ca0bfb9c3bb994b5191d63fa4b685
Jouni Kiviperä <jounikivipera@gmail.com>
2023-11-11 11:17:06 +0200

    m

--- JavaScript/Koodit/tehtäväsarja4/T1/kt1.js
+++ JavaScript/Koodit/tehtäväsarja4/T1/kt1.js
@@ -1,5 +1,5 @@
-function isLeapYear(year) {
-  if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+function isLeapYear(vuosi) {
+  if ((vuosi % 4 === 0 && vuosi % 100 !== 0) || vuosi % 400 === 0) {
     return true;
   } else {
     return false;
@@ -7,13 +7,22 @@ function isLeapYear(year) {
 }
 
 function checkYear() {
-  var year = document.getElementById("yearInput").value;
+  var vuosi = document.getElementById("yearInput").value;
   var resultDiv = document.getElementById("result");
 
-  if (isLeapYear(year)) {
-    resultDiv.innerText = "Year " + year + " is a leap year";
-  } else {
-    resultDiv.innerText = "Year " + year + " is not a leap year";
+  try {
+    if (isLeapYear(vuosi)) {
+      resultDiv.innerText = "Vuosi " + vuosi + " on karkausvuosi";
+    } else {
+      resultDiv.innerText = "Vuosi " + vuosi + " ei ole karkausvuosi";
+    }
+  } catch (e) {
+    // Virheen käsittely.
+    if (e.name === "TypeError") {
+      resultDiv.innerText = "Syötä kelvollinen vuosi.";
+    } else {
+      resultDiv.innerText = "Tapahtui virhe: " + e.message;
+    }
   }
 }
 
